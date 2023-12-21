3a5bd25befd3285e16bb1bba9b02b4ea9cf4b2ab
Tyree Zacharopoulos <tyree@tyree.ca>
2023-12-21 02:36:00 -0800

    Add TLS 1.3 cipher suites to tlsConfig

--- main.go
+++ main.go
@@ -61,6 +61,18 @@ func main() {
 
     tlsConfig.MinVersion = tls.VersionTLS12
     tlsConfig.MaxVersion = tls.VersionTLS13
+	tlsConfig.CipherSuites = []uint16{
+		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+    	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+    	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+    	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+    	tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+    	tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+    	// Optional: TLS 1.3 cipher suites - these are usually enabled by default
+    	tls.TLS_AES_128_GCM_SHA256,
+    	tls.TLS_AES_256_GCM_SHA384,
+    	tls.TLS_CHACHA20_POLY1305_SHA256,
+	}
 
 	server := &http.Server{
 		Addr:      ":https",
